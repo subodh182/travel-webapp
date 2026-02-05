aa5a7d2e579a1842146bc86d2ef1dd52d805e71c
Subodh Singh <subodhsin7417@gmail.com>
2026-02-05 20:46:24 +0530

    linking issues fixed css and js and img

--- app.py
+++ app.py
@@ -3,8 +3,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/service')
+@app.route('/service.html')
+def service():
+    return render_template('service.html')
+
+@app.route('/destination')
+@app.route('/destination.html')
+def destination():
+    return render_template('destination.html')
+
+@app.route('/contact')
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
